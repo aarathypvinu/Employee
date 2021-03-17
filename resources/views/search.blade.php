1aaf1adc15088f3b0538dee3ab91257c1e2ab77f
aarathypvinu <aarathypvinu@gmail.com>
2021-03-17 14:17:44 +0530

    nav

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -8,6 +8,25 @@
     <title>search</title>
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/employee">Employee</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/search">Search</a>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
 <div class="container">
 <div class="row">
 <div class="col col-12 col-sm-2 col-md-2 col-lg-2 col-xl-2 col-xxl-2"></div>
