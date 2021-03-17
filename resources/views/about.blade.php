f18651bca8d8e33cc0020db0fc259039a1b77709
aarathypvinu <aarathypvinu@gmail.com>
2021-03-17 15:33:27 +0530

    about us

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+@extends("theme")
+@section("content")
+<div class="container">
+<div class="row">
+<div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates beatae ipsam molestias architecto quo est repellendus quas accusantium omnis sunt! Voluptatum ex excepturi provident tempore quisquam nulla porro modi totam.
+</div>
+<div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus provident itaque ad! Quaerat repellendus, voluptatem velit accusantium harum tempora totam quis quam, aperiam vero quo libero ipsam ut porro optio?
+</div>
+</div>
+</div>
+
+
+
+@endsection
